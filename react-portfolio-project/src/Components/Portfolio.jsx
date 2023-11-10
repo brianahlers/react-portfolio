3bcd7c78a8310970ce0981a4f304c2c0267cad9f
Brian A <brian.ahlers@gmail.com>
2023-11-09 19:34:37 -0800

    added images to portfolio projects

--- react-portfolio-project/src/Components/Portfolio.jsx
+++ react-portfolio-project/src/Components/Portfolio.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 import lloydchristmas from '/images/lloydchristmas.png'
+import jd from '/images/jd.png'
+import moh from '/images/moh.gif'
+
 
 const Portfolio = () => {
   const projects = [
     { title: 'project 1', description: 'the first project', imgsrc: lloydchristmas },
-    { title: 'project 2', description: 'the second project' },
-    { title: 'project 3', description: 'the third project' }
+    { title: 'project 2', description: 'the second project', imgsrc: jd },
+    { title: 'project 3', description: 'the third project', imgsrc: moh }
   ]
   return (
     <div className='Portfolio'>
