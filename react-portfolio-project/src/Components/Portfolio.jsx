56419ecf07aa85624ae4574f8bc5aeb6359326fe
Brian A <brian.ahlers@gmail.com>
2023-11-09 19:01:54 -0800

    picture on portfolio page works

--- react-portfolio-project/src/Components/Portfolio.jsx
+++ react-portfolio-project/src/Components/Portfolio.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
-import ExamplePic from '../images/ExamplePic.jpg'
+import lloydchristmas from '/images/lloydchristmas.png'
 
 const Portfolio = () => {
   const projects = [
-    {title: 'project 1', description:'the first project', imgsrc: ExamplePic}, 
-    {title: 'project 2', description: 'the second project'}, 
-    {title: 'project 3', description: 'the third project'}
+    { title: 'project 1', description: 'the first project', imgsrc: lloydchristmas },
+    { title: 'project 2', description: 'the second project' },
+    { title: 'project 3', description: 'the third project' }
   ]
-    return (
-        <div className ='Portfolio'>
-        <h2>Portfolio</h2>
-        <div className='flex'>
-          {/* this box is going to hold the comment cards */}
-          {projects.map((project) => (
-            <ProjectCard title={project.title} description={project.description} imgsrc={imgsrc} />
-          ))}
-        </div>
+  return (
+    <div className='Portfolio'>
+      <h2>Portfolio</h2>
+      <div className='flex'>
+        {/* this box is going to hold the comment cards */}
+        {projects.map((project, index) => (
+          <ProjectCard title={project.title} description={project.description} imgsrc={project.imgsrc} key={index} />
+        ))}
       </div>
-    )
+    </div>
+  )
 }
 
 export default Portfolio;
