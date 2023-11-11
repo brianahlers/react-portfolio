bd0e5faab2a3fc1930273e74a5e7818c6ee39703
Brian A <brian.ahlers@gmail.com>
2023-11-11 00:49:32 -0800

    adding components to the website

--- react-portfolio-project/src/Components/Home.jsx
+++ react-portfolio-project/src/Components/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Card, Button } from 'react-bootstrap'
+import moh2 from '/images/moh2.gif'
+import tsvs2 from '/images/tsvs2.gif'
 
 const Home = () => {
-    return (
-        <div className ='Home'>
-        <h2>Home</h2>
-      </div>
-    )
+  return (
+    <div className='Home'>
+      <h2>Home</h2>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={moh2} />
+        <Card.Body>
+          <Card.Title>My Honest Reaction</Card.Title>
+          <Card.Text>
+            Honestly my reaction.
+          </Card.Text>
+          <Button variant="primary">Button</Button>
+        </Card.Body>
+      </Card>
+      <img src={tsvs2} />
+    </div>
+  )
 }
 
 export default Home;
