5fcc1599a8a5b7d42a625ecaff70300930998abd
Brian A <brian.ahlers@gmail.com>
2023-11-12 01:48:21 -0800

    tweaks

--- react-portfolio-project/src/Components/About.jsx
+++ react-portfolio-project/src/Components/About.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const About = () => {
-    return (
-        <div className ='About'>
-        <h2>About</h2>
-      </div>
-    )
+  return (
+    <div className='About'>
+      <h2>About</h2>
+      <p>
+        I have been on the hunt for knowledge,
+        new experiences, new friends,
+        and adrenaline from the jump.
+        Trying new things is 
+      </p>
+    </div>
+  )
 }
 
 export default About;
--- react-portfolio-project/src/Components/Portfolio.jsx
+++ react-portfolio-project/src/Components/Portfolio.jsx
@@ -3,13 +3,15 @@ import ProjectCard from './ProjectCard'
 import lloydchristmas from '/images/lloydchristmas.png'
 import jd from '/images/jd.png'
 import moh from '/images/moh.gif'
+import tsvs from '/images/tsvs.gif'
 
 
 const Portfolio = () => {
   const projects = [
     { title: 'project 1', description: 'the first project', imgsrc: lloydchristmas },
     { title: 'project 2', description: 'the second project', imgsrc: jd },
-    { title: 'project 3', description: 'the third project', imgsrc: moh }
+    { title: 'project 3', description: 'the third project', imgsrc: moh },
+    { title: 'project 4', description: 'the fourth project', imgsrc: tsvs}
   ]
   return (
     <div className='Portfolio'>
