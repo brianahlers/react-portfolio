94337abfa6e8bf4dc46fe8a0ab2fe21e6e126669
Brian A <brian.ahlers@gmail.com>
2023-11-30 16:06:27 -0800

    clean up

--- react-portfolio-project/src/Components/Home.jsx
+++ react-portfolio-project/src/Components/Home.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import { Card, Button } from 'react-bootstrap'
-import moh2 from '/images/moh2.gif'
-import tsvs2 from '/images/tsvs2.gif'
 
 const Home = () => {
   return (
